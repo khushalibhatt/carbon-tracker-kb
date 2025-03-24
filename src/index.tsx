87d51441f47668eaa171b8ced98bcdcdbb41576c
khushalibhatt <91697920+khushalibhatt@users.noreply.github.com>
2025-03-23 21:36:17 -0400

    Update index.tsx as a placeholder

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-// You can add additional imports as needed
-// import './index.css';
+//empty placeholder 
 
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Failed to find the root element');
@@ -14,10 +13,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you have any global code or service worker registration, add it here
-// For example:
-// reportWebVitals();
-
-// This empty export makes TypeScript treat this file as a module
-// You can remove this if you have other exports
 export {};
